4901bcbefc00f654890ddd5344e798249d104492
premsai <bpremsai1503@gmail.com>
2023-04-24 09:43:13 +0530

    Added Login Page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,28 +1,14 @@
 import React, { useState } from 'react';
+import App from '/home/prem/Desktop/Major_Project/Gramophone/frontend/src/App.js';
 
-
-function Login_page() {
-    
-    // this is for signIn and signUp buttons
-    const [showSignIn, setShowSignIn] = useState(true);
-    const [isLogged, setisLogged] = useState(false)
-
-    const handleSignUpClick = (event) => {
-        setShowSignIn(false);
-    };
-
-    const handleSignInClick = (event) => {
-        setShowSignIn(true);
-    };
-
-    // ends here
-
-
+function SignUpForm(props)
+{
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
     const [name, setName] = useState('');
-    const [confirmPass, setConfirmPass] = useState('')
+    const [confirmPass, setConfirmPass] = useState('');
 
     const handleUsernameChange = (event) => {
         setUsername(event.target.value);
@@ -32,7 +18,7 @@ function Login_page() {
         setPassword(event.target.value);
     };
 
-    const handlenameChange = (event) => {
+    const handleNameChange = (event) => {
         setName(event.target.value);
     };
 
@@ -40,81 +26,160 @@ function Login_page() {
         setConfirmPass(event.target.value);
     };
 
-    const handleSubmitSignIn = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('Form submitted:', username, password);
-        setisLogged(true)
-        setUsername('');
-        setPassword('');
+        props.onSignUp()
+        console.log("Press submit in signup");
         
-    };
-
-    const handleSubmitSignUp = (event) => {
-        event.preventDefault();
         if(password === confirmPass)
-            console.log('Form submitted:', name, username, password);
-        setUsername('');
-        setPassword('');
-        setName('');
-        setConfirmPass('');
-        setShowSignIn(true)
+        {
+            setUsername('');
+            setName('');
+            setPassword('');
+            setConfirmPass('');
+        }
+        else
+        {
+            setPassword('');
+            setConfirmPass('');
+            setError('Passwords do not match');
+        }
+            
+        // Handle sign up logic here
     };
-
-    const signIn = (
-        <form onSubmit={handleSubmitSignIn}>
-            <h1>Sign in</h1>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={handleUsernameChange} placeholder="Email or Phone"/>
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={handlePasswordChange} placeholder="Password"/>
-            </label>
-            <button type="submit">Sign in</button>
-      </form>
-    );
-
-    const signUp = (
-        <form onSubmit={handleSubmitSignUp}>
-            <h1>Sign up</h1>
-            <label>
-                Name:
-                <input type="text" value={name} onChange={handlenameChange} placeholder="Name"/>
-            </label>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={handleUsernameChange} placeholder="Email or Phone"/>
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={handlePasswordChange} placeholder="Password"/>
-            </label>
-            <label>
-                Confirm Password:
-                <input type="password" value={confirmPass} onChange={handleConfirmPassChange} placeholder="Password"/>
-            </label>
-            <button type="submit">Sign up</button>
-        </form>
-    );
-
-
-    
 
     return (
         <div>
-            {isLogged ? (
-                <h1>You are Logged in</h1>
-            ):(
-                <div>
-                    <button onClick={handleSignInClick}>Sign In</button>
-                    <button onClick={handleSignUpClick}>Sign Up</button>
-                    {showSignIn ? signIn : signUp}
-                </div>
-            )}
-            
+            <form onSubmit={handleSubmit}>
+                <h2>Sign up</h2>
+                <label>
+                    Name:
+                    <input 
+                        type="text" 
+                        value={name} 
+                        onChange={handleNameChange}
+                        placeholder='Name'
+                        required
+                    />
+                </label>
+                <label>
+                    Username:
+                    <input 
+                        type="text" 
+                        value={username} 
+                        onChange={handleUsernameChange} 
+                        placeholder='Email or Phone'
+                        required
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input 
+                        type="password" 
+                        value={password} 
+                        onChange={handlePasswordChange} 
+                        placeholder='Password'
+                        required
+                    />
+                </label>
+                <label>
+                    Confirm Password:
+                    <input 
+                        type="password" 
+                        value={confirmPass} 
+                        onChange={handleConfirmPassChange} 
+                        placeholder='Confirm Password'
+                        required
+                    />
+                </label>
+                <button type="submit" >Sign up</button>
+            </form>
+            {error && (<p>{error}</p>)}
         </div>
     );
 }
 
-export default Login_page;
+function SignInForm(props)
+{
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.onSignIn();
+        console.log("Press submit in signIN");
+        // Handle sign in logic here
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Sign in</h2>
+            <label>
+                Username:
+                <input 
+                    type="text" 
+                    value={username} 
+                    onChange={handleUsernameChange}
+                    placeholder='Email or Phone'
+                    required
+                />
+            </label>
+            <label>
+                Password:
+                <input 
+                    type="password" 
+                    value={password} 
+                    onChange={handlePasswordChange}
+                    placeholder='Password'
+                    required
+                />
+            </label>
+            <button type="submit">Sign in</button>
+        </form>
+    );
+}
+
+function SignInSignUpPage() 
+{
+    const [showSignIn, setShowSignIn] = useState(true);
+    const [isLogged, setisLogged] = useState(false);
+
+    const handleSignUpClick = () => {
+        setShowSignIn(false);
+    };
+
+    const handleSignInClick = () => {
+        setShowSignIn(true);
+        //setisLogged(true);
+    };
+
+    const handleSignIn = () => {
+        setisLogged(true)
+    }
+    const handleLogout = () => {
+        setisLogged(false)
+    }
+
+    return (
+        <div>
+            {isLogged ? (<App Logout={handleLogout}/>
+            ):(
+                <div>
+                    <button onClick={handleSignInClick}>Sign In</button>
+                    <button onClick={handleSignUpClick}>Sign Up</button>
+                    {showSignIn ? (<SignInForm onSignIn={handleSignIn}/>) : (<SignUpForm onSignUp={handleSignUpClick}/>)}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default SignInSignUpPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Test from './Test'
+// import Test from './Test'
 import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Login /> */}
-    <Test/>
+    <Login />
+    {/* <Test/> */}
   </React.StrictMode>
 );
 
